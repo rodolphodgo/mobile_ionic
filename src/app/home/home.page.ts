e6b8aadb55e623ae6f1e0dbe42cc59fd868d5c01
Rodolpho Dantas Gouveia <rodolphogouveiageo@gmail.com>
2025-10-29 20:30:56 -0300

    CREATE login page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonContent, // Já existia
+  IonTitle,   // Adicionado para o título Bem-vindo(a)!
+  IonInput,   // Adicionado para os campos de texto
+  IonItem,    // Adicionado para envolver os inputs
+  IonButton,  // Adicionado para os botões
+  IonIcon     // Adicionado para os ícones
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  styleUrls: ['home.page.scss'], // Ainda aponta para o SCSS
+  imports: [
+    IonContent,
+    IonTitle,
+    IonInput,
+    IonItem,
+    IonButton,
+    IonIcon // Não esqueça de adicionar IonIcon aqui!
+  ],
 })
 export class HomePage {
   constructor() {}
